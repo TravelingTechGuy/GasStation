a5fc924be1eb5496974908056aec6e053a41dfb9
Guy Vider <guy@guyvider.com>
2020-08-20 16:59:14 -0700

    starting work on screen

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,12 +1,52 @@
 import React, {useState} from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { Button, View, StyleSheet, Text, Switch, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 import Colors from '../config/colors';
 
-export default ({navigation, route}) => 
-  <View style={styles.container}>
-    <Text style={styles.text}>Settings Screen</Text>
-  </View>;
+export default ({navigation, route}) => {
+  const [autoRefresh, setAutoRefresh] = useState<boolean>(false);
+  const [interval, setInterval] = useState<number>(10);
+  
+  const saveSettings = () => {
+    navigation.goBack()
+  };
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.topLeftButton} onPress={saveSettings}>
+        <Ionicons name="md-save" size={32} color={Colors.offWhite} />
+      </TouchableOpacity>
+      <Text style={styles.headerText}>Settings Screen</Text>
+      <View style={{flexDirection: 'column'}}>
+        <Text style={styles.text}>Would you like to auto refresh the prices:</Text>
+        <Switch
+          trackColor={{ true: Colors.green, false: Colors.red }}
+          thumbColor={autoRefresh ? Colors.green : Colors.red}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={() => setAutoRefresh(!autoRefresh)}
+          value={autoRefresh}
+          style={styles.switch}
+        />
+      </View>
+      {
+        autoRefresh
+        ?
+        <>
+          <Text style={styles.text}>Interval in minutes:</Text>
+          <TextInput 
+            style={styles.textInput}
+            onChangeText={text => setInterval(parseInt(text, 10) || 0)}
+            value={interval.toString() || '0'}
+            editable={autoRefresh}
+          />
+        </>
+        :
+        null
+      }
+    </View>
+  );
+};
 
 const styles =StyleSheet.create({
   container: {
@@ -17,7 +57,31 @@ const styles =StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText: {
+    fontSize: 27,
+    textAlign: 'center',
+    color: Colors.offWhite,
+    position: 'absolute',
+    top: 50
+  },
+  switch: {
+    backgroundColor: Colors.light
+  },
   text: {
     color: Colors.light,
+  },
+  textInput: {
+    height: 20,
+    borderStyle: 'solid',
+    borderColor: Colors.light,
+    borderWidth: 1,
+    color: Colors.light
+  },
+  topLeftButton: {
+    position: 'absolute',
+    top: 30,
+    left: 20,
+    height:32,
+    width:32
   }
 });
